f891ad3a94443cbea28c95757a0e7519028dda25
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-09-08 18:48:21 +0100

    Add capitalize function and detect tie games

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,44 @@
 // Creates possible options array
-let options = ["Rock", "Paper", "Scissors"];
+let options = ["rock", "paper", "scissors"];
+
+// Capitalizes first letter
+function Capitalize(string){
+   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
 // returns one of the possibilities at random
-function ComputerPlay(){
+function computerPlay(){
 
     // Creates random number between 1 and 3 inclusive to use as an index for the array
     let index = Math.floor(Math.random()*(options.length));
-    
+
     // Use the index to return the answer
     return options[index];
 }
+
+// Play one round of Rock Paper Scissors. Receive player and computer selections
+function playRound(playerSelection, computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+
+    // In case of same selection, return a draw
+    if (playerSelection === computerSelection){
+        return `You Draw! Both picked ${Capitalize(computerSelection)}. Sadge :( try again..`;
+    }
+
+    // Player chooses rock
+    if (playerSelection === options[0]){
+
+    } 
+    // PLayer chooses paper
+    else if (playerSelection === options[1]){
+    } 
+    // Player chooses scissors 
+    else {
+
+    }
+}
+
+const playerSelection = "ROck";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
