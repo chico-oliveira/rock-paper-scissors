5277565d029af550d39b77156561b85295e52e57
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-09-08 17:17:42 +0100

    Add computer selection function

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,12 @@
-console.log("Hello World....");
+// Creates possible options array
+let options = ["Rock", "Paper", "Scissors"];
+
+// returns one of the possibilities at random
+function ComputerPlay(){
+
+    // Creates random number between 1 and 3 inclusive to use as an index for the array
+    let index = Math.floor(Math.random()*(options.length));
+    
+    // Use the index to return the answer
+    return options[index];
+}
